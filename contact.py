e2cf9fba83584f462cd6d6c82c6ddf4f0d369043
la1i1u <mary.kashka@gmail.com>
2020-06-03 21:20:57 +0300

    add my first unittest

--- contact.py
+++ contact.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class Contact:
     def __init__(self, first_name, last_name, email, phone, job):
         self.first_name = first_name
@@ -8,3 +11,16 @@ class Contact:
 
     def __repr__(self):
         return "{" + self.first_name + " " + self.last_name + " " + self.email + " " + self.phone + " " + self.job + "}"
+
+
+class TestContact(unittest.TestCase):
+    def setUp(self):
+        self.arguments = ["first_name", "last_name", "email", "phone", "job"]
+        self.contact = Contact(*self.arguments)
+
+    def test_first_name_is_first(self):
+        self.assertEqual(self.contact.first_name, self.arguments[0], "халоу имя норм")
+
+
+if __name__ == '__main__':
+    unittest.main()
