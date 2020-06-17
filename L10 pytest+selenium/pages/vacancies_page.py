1761aadfa26d539196169324c96fe46480fb7a8e
la1i1u <mary.kashka@gmail.com>
2020-06-17 22:26:02 +0300

    add new locators second test new def for works with vacancies page

--- L10 pytest+selenium/pages/vacancies_page.py
+++ L10 pytest+selenium/pages/vacancies_page.py
@@ -5,11 +5,30 @@ from selenium.webdriver.common.by import By
 
 
 class VacanciesLocators:
+
+    # Кнопки
     BUTTON_SEARCH = (By.XPATH, '//button[contains(text(), "Найти")]')
+    BUTTON_ADD_VACANCIES = (By.XPATH, '//button[contains(text(), "+ Опубликовать вакансию бесплатно")]')
+    BUTTON_SHOW_CONTACTS = (By.XPATH, '//button[contains(text(), "Показать контакты")]')
+    BUTTON_FEEDBACK = (By.XPATH, '//button[contains(text(), "Что вы думаете об этом разделе?")]')
+    BUTTON_MORE_DESCRIPTION = (By.XPATH, '//button//span[contains(text(), "Развернуть описание")]')
+    BUTTON_LESS_DESCRIPTION = (By.XPATH, '//button//span[contains(text(), "Свернуть описание")]')
+
+    # Поисковые элементы
     INPUT_FOR_COMPANY_NAME = (By.XPATH, '//input[@placeholder="Название компании"]')
-    COMPANY_NAMES = (By.XPATH, '//a[contains(@href,"company")]')
     INPUT_FOR_PRICE_FROM = (By.XPATH, '//div[span[contains(text(), "Зарплата")]]//input[@placeholder="от"]')
     INPUT_FOR_PRICE_TO = (By.XPATH, '//div[span[contains(text(), "Зарплата")]]//input[@placeholder="до"]')
+    INPUT_FOR_REGION = (By.XPATH, '//div[contains(@class,"region")]//input')
+    SEARCH_POPUP_WITH_REGIONS = (By.XPATH, '//div[contains(@class,"popup_content")]//div')
+    SEARCH_REGION = '//div[contains(@class,"popup_content")]//div[contains(text(), "{}")]'
+
+
+
+    # Элементы на выдаче
+    COMPANY_NAMES = (By.XPATH, '//a[contains(@href,"company")]')
+    REGIONS_ON_RESULT = (By.XPATH, '//div[contains(text(), "Город")]/following-sibling::div[contains(@class, '
+                                   '"bold_text")]')
+    PHONE = (By.XPATH, '//div[contains(@class, "phone")]')
 
 
 class VacanciesPage(BasePage):
@@ -17,11 +36,11 @@ class VacanciesPage(BasePage):
         super().__init__(driver)
         self.vacancies_url = 'vacancies/'
 
-    #переход на страницу вакансий
+    # Переход на страницу вакансий
     def go_to_vacancies(self):
         return self.driver.get(self.base_url + self.vacancies_url)
 
-    #ввод значений в поиск
+    # Ввод значений в поиск
     def enter_word_in_company_names_input(self, word):
         search_field = self.find_element(VacanciesLocators.INPUT_FOR_COMPANY_NAME)
         search_field.clear()
@@ -40,16 +59,36 @@ class VacanciesPage(BasePage):
         price_to.send_keys(price)
         return price_to
 
-    def choose_city(self):
-        print('булочки')
+    def enter_region_for_keys(self,selected_region):
+        region = self.find_element(VacanciesLocators.INPUT_FOR_REGION)
+        region.clear()
+        region.send_keys(selected_region)
+        return region
 
-    #кнопка поиска
+    # Поиск элемента в попапе региона
+    def click_on_needed_region(self, region):
+        locator = (By.XPATH, VacanciesLocators.SEARCH_REGION.format(region))
+        needed_region = self.find_element(locator)
+        needed_region.click()
+
+    #  Кнопка поиска
     def click_on_search_button(self):
         return self.find_element(VacanciesLocators.BUTTON_SEARCH).click()
 
-    #поиск элементов в выдаче
+    # Поиск элементов в выдаче
     def find_all_company_names(self):
         result = [item.text for item in self.find_elements(VacanciesLocators.COMPANY_NAMES)]
+        return result
+
+    def find_all_regions(self):
+        result = [item.text for item in self.find_elements(VacanciesLocators.REGIONS_ON_RESULT)]
+        return result
+
+    def click_on_show_contacts(self):
+        return self.find_element(VacanciesLocators.BUTTON_SHOW_CONTACTS).click()
+
+    def find_phone(self):
+        result = self.find_element(VacanciesLocators.PHONE)
         return result
 
 
--- L10 pytest+selenium/tests/test_vac0002.py
+++ L10 pytest+selenium/tests/test_vac0002.py
@@ -5,6 +5,20 @@ from pages.vacancies_page import VacanciesPage
 
 class TestVacation:
 
+    def test_choose_region(self,driver):
+        needed_region = 'Кубинка'
+        vacancies_main_page = VacanciesPage(driver)
+        vacancies_main_page.go_to_vacancies()
+        vacancies_main_page.enter_region_for_keys(needed_region)
+        vacancies_main_page.click_on_needed_region(needed_region)
+        vacancies_main_page.click_on_search_button()
+        result = vacancies_main_page.find_all_regions()
+        for i in result:
+            print("нашла")
+            assert i.find(needed_region) or i == needed_region
+        vacancies_main_page.click_on_show_contacts()
+        assert vacancies_main_page.find_phone()
+
     def test_search(self, driver):
         # constants
         word = 'Центральный'
